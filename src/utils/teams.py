b3ece0fa49203bb6a5093e839147f3b3e2b24d1b
sam <sunkao1035@gmail.com>
2023-05-17 16:45:40 +0800

    feat: update-utils

--- src/utils/teams.py
+++
@@ -1,8 +0,0 @@
-"""Functions of teams operation."""
-from prefect.blocks.notifications import MicrosoftTeamsWebhook
-
-
-def send_message_to_teams(channel: str, message: str) -> None:
-    """Send message to teams."""
-    teams_webhook_block = MicrosoftTeamsWebhook.load(channel)
-    teams_webhook_block.notify(message)
--- src/utils/utils/gcp/__init__.py
+++
@@ -1,1 +0,0 @@
-"""All Google Platform related utils."""
