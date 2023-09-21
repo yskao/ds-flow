7d7d879ed7677634bc04adc536edc751862ae918
samkao <sunkao1035@gmail.com>
2023-09-21 11:21:03 +0800

    update

--- src/mllib/sql_query/word_pos_ner_script.py
+++ src/mllib/sql_query/word_pos_ner_script.py
@@ -4,7 +4,7 @@ def create_ads_survey_answers_word_segmentation():
     """Create DS.ads_survey_answers_word_segmentation."""
     bq_query = """
         CREATE OR REPLACE TABLE DS.ads_survey_answers_word_segmentation (
-            assess_date DATE OPTIONS(description="模型執行時間")
+            assess_date STRING OPTIONS(description="模型執行時間"),
             answer_id STRING OPTIONS(description="問題代號"),
             ws STRING OPTIONS(description="斷詞"),
             pos STRING OPTIONS(description="詞性標記"),
@@ -17,7 +17,7 @@ def create_ads_survey_answers_named_entity_recognition():
     """Create DS.ads_survey_answers_named_entity_recognition."""
     bq_query = """
         CREATE OR REPLACE TABLE DS.ads_survey_answers_named_entity_recognition (
-            assess_date DATE OPTIONS(description="模型執行時間")
+            assess_date STRING OPTIONS(description="模型執行時間"),
             answer_id STRING OPTIONS(description="問題代號"),
             words STRING OPTIONS(description="字詞"),
             ners STRING OPTIONS(description="實體辨識,如人或物品"),
